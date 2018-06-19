077101ea6f1ac5695ce9dea8cc1b4d3654a6d490
benjaminbarbe10 <benjamin.barbe@viacesi.fr>
2018-06-20 00:46:37 +0200

    Installed all dependencies: redux / form / material / mdl / dom / router / thunk / axios Proxy defined Skeleton done Removed unused files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./assets/stylesheets/index.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>portfolio ben</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
